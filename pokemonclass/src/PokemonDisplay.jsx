1de29c129ab7dee262ca5f29ea3d00e1365cfa7c
NickSpringall <123866019+NickSpringall@users.noreply.github.com>
2023-09-16 11:38:28 +1000

    feature pokemon data being fetched

--- pokemonclass/src/PokemonDisplay.jsx
+++ pokemonclass/src/PokemonDisplay.jsx
@@ -11,10 +11,52 @@ export default class PokemonDisplay extends React.Component{
         }
     }
 
+    async componentDidMount(){
+        console.log("This message will appear once");
+
+        // grab a pokemon and store its name
+        // Retrieve the API data
+	let response = await fetch("https://pokeapi.co/api/v2/pokemon/" + getRandomPokemonId()).catch(error => {
+		throw new Error("API failure.");
+	});
+    console.log("fetch has finished")
+
+	if (response.status === 404){
+		throw new Error("API did not have data for the requested ID.");
+	}
+
+	// Convert the response into usable JSON 
+	let data = await response.json().catch(error => {
+		throw new Error("API did not return valid JSON.");
+	});
+
+    // Save pokemon name to component state
+    this.setState({PokemonName: data.name})
+
+        // Generate a random number or usre a user-provided number
+        function getRandomPokemonId(){
+            return Math.floor(Math.random() * 1010) + 1
+        }
+    }
+
+
+
+    componentWillUnmount(){
+        console.log("Component is being removed from the page")
+    }
+
     render(){
+
+        if(this.state.PokemonName) {
+            return (
+                <div>
+                    <h1>{this.state.PokemonName}</h1>
+                </div>
+            )
+        }
         return(
             <div>
-                <h1>Some content here</h1>
+                <h1>Loading...</h1>
             </div>
         )
     }
